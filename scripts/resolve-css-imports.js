3cf248bd36471b3bf5b0694ca81ade0f538d98e6
Ty Hopp <hopp.ty.c@gmail.com>
2021-05-22 14:16:57 +0800

    Resolve css imports script

---
+++ scripts/resolve-css-imports.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const entry = path.resolve('dist/styles/main.css');
+
+let main = fs.readFileSync(entry).toString();
+
+function resolveImports(main) {
+  const firstImport = main.match(/@import\s'(.*?)';/);
+
+  if (!firstImport) {
+    return main;
+  }
+
+  const firstImportContent = fs.readFileSync(
+    path.resolve('dist/styles/', firstImport[1])
+  );
+
+  main = main.replace(firstImport[0], firstImportContent);
+  return resolveImports(main);
+}
+
+const resolvedMain = resolveImports(main);
+
+fs.writeFileSync(path.resolve(entry), resolvedMain);
